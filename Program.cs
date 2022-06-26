53d90a142caa6189cb07bfb01d19adc89e4f7ce7
unknown <metallmuff@gmail.com>
2022-06-26 19:17:16 +1000

    первая задача

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.*/
+
+/*
+int number = new Random().Next(100,1000);
+Console.WriteLine("Случайное число : " + number ) ;
+int digitOne = number %100;
+int digitTwo = digitOne / 10;
+
+int result = digitTwo;
+
+Console.WriteLine("нужное число : " +result)
+*/
+////////////конец первой задачи///////
